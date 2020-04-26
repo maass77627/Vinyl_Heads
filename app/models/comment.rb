084ca65cbf77d91abef7ba78a63d8ad4b202090e
kelly_maass <kmaass77627@gmail.com>
2020-04-26 07:55:13 -0500

    functioning comments

--- app/models/comment.rb
+++ app/models/comment.rb
@@ -2,6 +2,18 @@ class Comment < ApplicationRecord
     belongs_to :user #optional: true
     belongs_to :post
     validates :message, presence: true, allow_blank: false
+    accepts_nested_attributes_for :user, reject_if: proc { |attributes| attributes['username'].blank?}
+
+  def user_attributes(user_attributes)
+    user_attributes.values.each do |user_attributes|
+      if user_attributes[:username].present?
+        user = User.find_or_create_by(user_attributes)
+        if !self.user = user
+          self.build_user(:username => user[:username])
+        end
+      end
+    end
+  end
 
     def post_ids=(ids)
       ids.each do |id|
--- app/models/post.rb
+++ app/models/post.rb
@@ -10,7 +10,7 @@ class Post < ApplicationRecord
     has_many :comments
    # has_many :genres, inverse_of: :post
     has_many :users, through: :comments
-    accepts_nested_attributes_for :record, :genres#, :record #genre plural
+    accepts_nested_attributes_for :record#, :genres#, :record #genre plural
     has_one_attached :image
     #has_many :user_posts
    # has_many :users, through: :user_posts
@@ -18,10 +18,10 @@ class Post < ApplicationRecord
    has_many :genres, through: :post_genres
 
       
-  def categories_attributes=(category_attribute)
-    category_attributes.values.each do |category_attribute|
-      category = Category.find_or_create_by(category_attribute)   ##??
-      self.post_categories.build(category: category)
+  def genre_attributes=(genre_attribute)
+    genre_attributes.values.each do |genre_attribute|
+      genre = Genre.find_or_create_by(genre_attribute)   ##??
+      self.post_genres.build(genre: genre)
     end
   end
 
