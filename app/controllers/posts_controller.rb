df38c0e02af5fca4e6e7814b504056409af63d5a
kelly_maass <kmaass77627@gmail.com>
2020-05-03 14:27:26 -0500

    fixes

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,42 +1,41 @@
 class PostsController < ApplicationController
   
-      def index
+    def index
       if params[:genre_id]
         @genre = Genre.find_by(id: params[:genre_id])
-          if @genre.posts.empty?
-            flash[:alert] = "This genre has no posts. Please add one below."
-            redirect_to new_genre_path(@genre)
-          else
-            @posts = @genre.posts
-          end
+      if @genre.posts.empty?
+        flash[:alert] = "This genre has no posts. Please add one below."
+        redirect_to new_genre_path(@genre)
+      else
+        @posts = @genre.posts
+      end
       else
         @posts = Post.all
       end
     end
 
     def new
-         @post = Post.new
-         @post.user = current_user
+        @post = Post.new
+        @post.user = current_user
         @record = @post.build_record(title: "title",
         id: :id)
         @artist = @post.build_artist(name: "name",
         id: :id)
-       @genres = @post.genres.build([
+        @genres = @post.genres.build([
         { name: "test", id: [] }])
-        end
+    end
         
  
     def create
           @post = Post.create(post_params)
-          binding.pry
           @post.user = current_user
           @post.image.attach(params[:post][:image])
         if @post.save
            redirect_to post_path(@post)
         else 
             render :new 
-          end
         end
+    end
 
     
     def edit
@@ -44,7 +43,7 @@ class PostsController < ApplicationController
     end
 
     def update
-        @post = Post.find(params[:id])
+         @post = Post.find(params[:id])
       if @post.update(post_params)
          redirect_to @post
       else
@@ -57,8 +56,8 @@ class PostsController < ApplicationController
     end
     
     def destroy
-     @post = Post.find(params[:id])
-     @post.destroy
+      @post = Post.find(params[:id])
+      @post.destroy
       redirect_to users_path(@user)
     end 
 
--- app/models/post.rb
+++ app/models/post.rb
@@ -1,46 +1,25 @@
 class Post < ApplicationRecord
   validates_associated :user
   validates_associated :record
-  validates_associated :genres #######
+  validates_associated :genres
   validates_associated :artist
-    belongs_to :user#, optional: true
-    has_many :post_genres   ###
-    has_many :genres, through: :post_genres ####
+    belongs_to :user
+    has_many :post_genres
+    has_many :genres, through: :post_genres
     belongs_to :artist
     has_one :record
     has_many :comments
     has_many :users, through: :comments
-    accepts_nested_attributes_for :record, :genres, :artist #####
+    accepts_nested_attributes_for :record, :genres, :artist
     has_one_attached :image
     has_many :post_categories
     has_many :categories, through: :post_categories
-
-   # has_many :users_posts
-   # has_many :users, through :users_posts
     
-
     def self.by_genre(genre_id)
       where(genre: genre_id)
     end
-  
-    def genre_name=(name)
-      self.genre = Genre.find_or_create_by(name: name)
-    end
-  
-    def genre_name
-       self.genre ? self.genre.name : nil
-    end
-
-    def artist_name=(name)
-      self.artist = Artist.find_or_create_by(name: name)
-    end
-  
-    def artist_name
-       self.artist ? self.artist.name : nil
-    end
     
-
-       def user_ids=(ids)
+      def user_ids=(ids)
        ids.each do |id|
        user = User.find(id)
          self.users << user
